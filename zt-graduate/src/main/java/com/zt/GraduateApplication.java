633d96e90bcaf7e5c93ca37bbdfdd1f192319b0a
Yufy <1002372219@qq.com>
2023-05-22 22:10:53 +0800

    init

---
+++ zt-graduate/src/main/java/com/zt/GraduateApplication.java
@@ -0,0 +1,11 @@
+package com.zt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraduateApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GraduateApplication.class, args);
+    }
+}
